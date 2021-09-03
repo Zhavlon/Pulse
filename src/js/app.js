01527774ea34a0515d4e7ac1447f4633128581fe
Zhavlon <javlonadakhamov@gmail.com>
2021-09-03 18:01:36 +0600

    created modals and made them responsive

--- src/js/app.js
+++ src/js/app.js
@@ -33,5 +33,21 @@ $(document).ready(function () {
 		})
 	}
 
+	//modal
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow')
+	})
+
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+	})
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__subtitle').text($('.catalog-item__title').eq(i).text())
+			$('.overlay, #order').fadeIn('slow')
+		})
+	})
 
 });
